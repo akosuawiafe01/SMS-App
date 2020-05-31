1b6466f23069fc08d71f977b9bc163eb05d2b3f1
akosuawiafe01 <wiafenyarkoa@gmail.com>
2020-05-31 02:03:19 +0000

    Initial Commit

---
+++ SMS-App/Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class IndexForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.StudentToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CourseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GradeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReportToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StudentToolStripMenuItem, Me.CourseToolStripMenuItem, Me.GradeToolStripMenuItem, Me.ReportToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'StudentToolStripMenuItem
+        '
+        Me.StudentToolStripMenuItem.Name = "StudentToolStripMenuItem"
+        Me.StudentToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.StudentToolStripMenuItem.Text = "Student"
+        '
+        'CourseToolStripMenuItem
+        '
+        Me.CourseToolStripMenuItem.Name = "CourseToolStripMenuItem"
+        Me.CourseToolStripMenuItem.Size = New System.Drawing.Size(56, 20)
+        Me.CourseToolStripMenuItem.Text = "Course"
+        '
+        'GradeToolStripMenuItem
+        '
+        Me.GradeToolStripMenuItem.Name = "GradeToolStripMenuItem"
+        Me.GradeToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.GradeToolStripMenuItem.Text = "Grade"
+        '
+        'ReportToolStripMenuItem
+        '
+        Me.ReportToolStripMenuItem.Name = "ReportToolStripMenuItem"
+        Me.ReportToolStripMenuItem.Size = New System.Drawing.Size(54, 20)
+        Me.ReportToolStripMenuItem.Text = "Report"
+        '
+        'IndexForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.IsMdiContainer = True
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "IndexForm"
+        Me.Text = "Student Management System"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents StudentToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CourseToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GradeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ReportToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ SMS-App/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class IndexForm
+    Private Sub StudentToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles StudentToolStripMenuItem.Click
+        Dim studentFrm As New frmStudent
+        studentFrm.MdiParent = Me
+        studentFrm.Show()
+    End Sub
+
+    Private Sub CourseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CourseToolStripMenuItem.Click
+        Dim courseFrm As New frmCourse
+        courseFrm.MdiParent = Me
+        courseFrm.Show()
+    End Sub
+End Class
